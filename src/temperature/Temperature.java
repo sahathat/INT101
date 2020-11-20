8c9ed83002428fddab9d61aa6116b7b1d5ed070b
sahat <sahat@DESKTOP-2N12NJ5>
2020-11-20 18:32:19 +0700

    fix problem comment and average temperature

--- src/temperature/Temperature.java
+++ src/temperature/Temperature.java
@@ -1,24 +1,39 @@
+/**
+ * <h1>Temperature</h1>
+ *
+ * -set double variable air and dirt temperature of plant
+ *
+ * -set status air and dirt temperature of plant
+ * -condition with 2 time of air temperature and 3 time of dirt temperature divid by mutiple of 5
+ *
+ * -show status
+ */
 
 package temperature;
 
 public class Temperature {
+    //initial value
     private final double tempair;
     private final double tempdirt;
     private TempStatus tempstatus = null;
-    
+
+    //constructor
     public Temperature(double tempair, double tempdirt) {
         this.tempair = tempair;
         this.tempdirt = tempdirt;
+        //get status of temperature
         getStatus(tempair,tempdirt);
     }
-    
+
+    //getter value tempair and tempdirt
     public double getTempair(){
         return tempair;
     }
     public double getTempdirt(){
         return tempdirt;
     }
-    
+
+    //condition status of tempair and tempdirt
     public TempStatus getStatus(double tempair,double tempdirt){
     if((2*(tempair)+3*(tempdirt))/(5) >= 35) {
         this.tempstatus = TempStatus.VERYHOT;
@@ -33,7 +48,8 @@ public class Temperature {
     }
         return tempstatus;
     }
-    
+
+    //toString value
     @Override
     public String toString() {
         return "Temperature{ tempair = " + tempair + " celcius , tempdirt = " + tempdirt + " celcius , status = " + tempstatus + " }";
--- src/temperature/tempTest.java
+++ src/temperature/tempTest.java
@@ -3,8 +3,9 @@ package temperature;
 
 public class tempTest {
     public static void main(String[] args) {
-        double[] tempair = {40.0,28.0,22.0};
-        double[] tempdirt = {36.0,28.0,20.0}; 
+        //data type array is tempair and tempdirt
+        double[] tempair = {30.0,28.0,22.0};
+        double[] tempdirt = {36.0,28.0,20.0};
         TempAnalysis test0 = new TempAnalysis(6);
         test0.add(tempair[0], tempdirt[0]);
         test0.add(tempair[1], tempdirt[1]);
